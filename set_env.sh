4117862b7c2c34d8bfe33bcdf299a8e2debdc7c2
sonbz <nguyentason.13021998@gmail.com>
2022-03-14 18:24:42 +0700

    14/03/2022

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=son212612323428
+export POSTGRES_PASSWORD=son212612323428
+export POSTGRES_HOST=son212612323428.czon5aqoog5k.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=son212612323428
+export AWS_BUCKET=arn:aws:s3:::project3-212612323428-nguyenson
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=nguyenson212612323428
+export URL=http://localhost:8100
