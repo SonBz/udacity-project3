d6fc0e71b5fb04f29d9c7dfcd4fee54f019beab5
sonbz <nguyentason.13021998@gmail.com>
2022-03-17 10:49:27 +0700

    17032022

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae8ae37bcba43439c824518f6f8d2b59-87378022.us-east-1.elb.amazonaws.com/'
 };
 
 /*
